e6b6c7e6fca5abbec63b8a52669d17cd36248bc0
Dina Polousova <dinapolousova@gmail.com>
2023-06-18 10:15:29 +0300

    pow x<=5

--- test.js
+++ test.js
@@ -1,5 +1,25 @@
 describe("pow", function() {
-    it ("возводим в степень n", function() {
-        assert.equal(pow(2, 3), 8); 
+    // it ("2 в степени 3 будет 8", function() {
+    //     assert.equal(pow(2, 3), 8); 
+    // });
+
+    // it ("3 в степени 3 будет 27", function() {
+    //     assert.equal(pow(3, 3), 27); 
+    // });
+    describe("возводит x в степень 3", function() {
+
+        function makeTest(x) {
+            let expected = x * x * x;
+            it (`${x} в степени 3 будет ${expected}`, function() {
+                assert.equal(pow(x, 3), expected);
+            });
+        }
+
+        for (let x = 1; x <= 5; x++) {
+            makeTest(x);
+        }
     });
-});
+        
+    
+    });
+
