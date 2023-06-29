1020a668fd82563c9e157d6d32271121dd74c96c
Dina Polousova <dinapolousova@gmail.com>
2023-06-29 23:33:33 +0300

    calc

--- test.js
+++ test.js
@@ -32,15 +32,6 @@ calculator.read();
 console.log( "Sum=" + calculator.sum() );
 console.log( "Mul=" + calculator.mul() );
 
-// describe("validate", function() {
-//     function makeTest(x) {
-//         let expected = prompt ("Введите число > 0");
-//         it ("") {
-//             assert 
-//         }
-//     }
-// });
-
 // describe("pow", function() {
 //     // it ("2 в степени 3 будет 8", function() {
 //     //     assert.equal(pow(2, 3), 8); 
